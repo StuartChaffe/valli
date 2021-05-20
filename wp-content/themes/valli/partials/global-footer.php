ae6a37848a2baf105951cd389da74c34e6606228
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-05-20 16:39:03 +0100

    Style footer

--- wp-content/themes/valli/partials/global-footer.php
+++ wp-content/themes/valli/partials/global-footer.php
@@ -1,20 +1,107 @@
 <footer class="global-footer">
 	<div class="global-footer__inner">
-		<p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-
-		<ul class="social-links">
-		<?php if(get_field('twitter', 'options')): ?>
-			<li class="social-links__item"><a href="<?php the_field('twitter', 'options'); ?>" class="social-links__link"><?php echo get_icon('twitter'); ?></a></li>
-		<?php endif; ?>
-		<?php if(get_field('facebook', 'options')): ?>
-			<li class="social-links__item"><a href="<?php the_field('facebook', 'options'); ?>" class="social-links__link"><?php echo get_icon('facebook'); ?></a></li>
-		<?php endif; ?>
-		<?php if(get_field('instagram', 'options')): ?>
-			<li class="social-links__item"><a href="<?php the_field('instagram', 'options'); ?>" class="social-links__link"><?php echo get_icon('instagram'); ?></a></li>
-		<?php endif; ?>
-		<?php if(get_field('youtube', 'options')): ?>
-			<li class="social-links__item"><a href="<?php the_field('youtube', 'options'); ?>" class="social-links__link"><?php echo get_icon('youtube'); ?></a></li>
-		<?php endif; ?>
-		</ul>
+		<div class="global-footer__logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.svg" alt="Valli Opticians Logo" /></a></div>
+		<div class="global-footer__locations">
+			<ul>
+				<li>
+					<p><strong>Almondbury</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Stockport</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Honley Holmfirth</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Hull</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+				<li>
+					<p><strong>Hyde</strong> <br />
+					Address line 1<br />
+					Address line 2<br />
+					Tel: <a href="tel:0800800800">0800 800 800</a><br />
+					<a href="mailto:email@valliopticians.co.uk">email@valliopticians.co.uk</a></p>
+				</li>
+			</ul>
+		</div>
+		<div class="global-footer__copy">
+			<p>&copy; Copyright <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
+			<ul>
+				<li><a href="">Privacy policy</a></li>
+				<li><a href="">Staff login</a></li>
+			</ul>
+		</div>
 	</div>
 </footer>
